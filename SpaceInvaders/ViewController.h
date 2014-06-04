437e0653a1f406ce97174e5e9ec35aa5102f465a
luowei <luowei010101@gmail.com>
2014-06-04 15:11:26 +0800

    Initial Commit

---
+++ SpaceInvaders/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SpaceInvaders
+//
+//  Created by luowei on 14-6-4.
+//  Copyright (c) 2014年 rootls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SpaceInvadersTests/SpaceInvadersTests.m
@@ -0,0 +1,34 @@
+//
+//  SpaceInvadersTests.m
+//  SpaceInvadersTests
+//
+//  Created by luowei on 14-6-4.
+//  Copyright (c) 2014年 rootls. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SpaceInvadersTests : XCTestCase
+
+@end
+
+@implementation SpaceInvadersTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
